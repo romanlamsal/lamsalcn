39dfd96fe5dd17bd861fe53047c2f14ac5246402
Roman Lamsal <roman@lamsal.de>
2025-11-23 14:42:44 +0100

    wip: refactor dep resolution

--- cli/add.ts
+++ cli/add.ts
@@ -1,45 +1,49 @@
-import {checkbox, confirm} from "@inquirer/prompts";
-import type {RegistryEntry} from "../registry";
-import process from "node:process";
-import {existsSync, mkdirSync, readFileSync, rmSync} from "node:fs";
-import {cwdPath} from "./cwd-path";
-import {CONFIG_FILENAME, type LamsalcnConfig} from "./config-file";
-import degit from "degit";
-import {readFile} from "node:fs/promises";
-import {execSync} from "node:child_process";
-import {getConflictingDeps} from "./get-conflicting-deps";
-import {join} from "node:path";
-import { randomUUID } from "node:crypto";
-import * as os from "node:os";
+import { execSync } from "node:child_process"
+import { randomUUID } from "node:crypto"
+import { existsSync, mkdirSync, readFileSync, rmSync, statSync } from "node:fs"
+import { readFile } from "node:fs/promises"
+import * as os from "node:os"
+import { join } from "node:path"
+import process from "node:process"
+import { checkbox, confirm } from "@inquirer/prompts"
+import degit from "degit"
+import type { RegistryEntry } from "../registry"
+import { CONFIG_FILENAME, type LamsalcnConfig } from "./config-file"
+import { cwdPath } from "./cwd-path"
+import { getConflictingDeps } from "./get-conflicting-deps"
 
-const configPath = cwdPath(CONFIG_FILENAME);
+const configPath = cwdPath(CONFIG_FILENAME)
 if (!existsSync(configPath)) {
     console.error(`Need to call "init" first.`)
-    process.exit(1);
+    process.exit(1)
 }
 
 const force = process.argv.includes("--force")
 const verbose = process.argv.includes("--verbose")
 
-const lamsalCnConfig = JSON.parse(readFileSync(configPath, "utf8")) as LamsalcnConfig;
+const lamsalCnConfig = JSON.parse(readFileSync(configPath, "utf8")) as LamsalcnConfig
 
 const registry = await fetch("https://raw.githubusercontent.com/romanlamsal/lamsalcn/refs/heads/main/registry.json")
-    .then(res => res.json() as Promise<RegistryEntry[]>).catch((err) => {
+    .then(async res => {
+        const json = (await res.json()) as RegistryEntry[]
+        return json.sort((a, b) => a.name.localeCompare(b.name))
+    })
+    .catch(err => {
         console.error("Could not fetch registry:", err)
-        process.exit(1);
-    });
+        process.exit(1)
+    })
 
 async function getAdded(): Promise<string[]> {
-    const addPos = process.argv.indexOf('add')
+    const addPos = process.argv.indexOf("add")
 
     const added = process.argv.slice(addPos + 1, process.argv.length)
 
-    const registeredSources = registry.map(regEntry => regEntry.name);
+    const registeredSources = registry.map(regEntry => regEntry.name)
     if (added.length) {
         const notFound = added.filter(candidate => !registeredSources.includes(candidate))
         if (notFound.length) {
             console.log(`Unknown option${notFound.length !== 1 ? "s" : ""}: "${notFound.join(", ")}"`)
-            process.exit(1);
+            process.exit(1)
         }
 
         return added
@@ -47,7 +51,8 @@ async function getAdded(): Promise<string[]> {
 
     try {
         return checkbox({
-            message: "Code to add", choices: registeredSources,
+            message: "Code to add",
+            choices: registeredSources,
         })
     } catch (err) {
         if (err instanceof Error && err.name === "ExitPromptError") {
@@ -57,15 +62,37 @@ async function getAdded(): Promise<string[]> {
     }
 }
 
-const added = await getAdded();
+const added = await getAdded()
 console.log("Adding:", added)
 
-async function copySources(added: string[]) {
-    const { repository } = await import("../package.json");
+async function clone(entry: string, cb: (outputLocation: string) => Promise<void>): Promise<void> {
+    const { repository } = await import("../package.json")
 
-    const packageJson = await readFile(cwdPath("package.json"), "utf8").then((contents) => {
+    const emitter = degit(join(repository, entry), {
+        cache: false,
+        force,
+        verbose,
+    })
+
+    const id = randomUUID()
+    const degitOutput = join(os.tmpdir(), `lamsalcn-${id}`)
+
+    try {
+        await emitter.clone(degitOutput)
+
+        await cb(degitOutput)
+    } finally {
+        rmSync(degitOutput, { recursive: true, force: true })
+    }
+}
+
+async function copySources(added: string[]) {
+    const packageJson = await readFile(cwdPath("package.json"), "utf8").then(contents => {
         try {
-            return JSON.parse(contents) as { dependencies?: Record<string, string>, devDependencies?: Record<string, string> };
+            return JSON.parse(contents) as {
+                dependencies?: Record<string, string>
+                devDependencies?: Record<string, string>
+            }
         } catch {
             return {}
         }
@@ -75,21 +102,61 @@ async function copySources(added: string[]) {
     const overallDevDeps = []
 
     for (const regEntryName of added) {
-        const config: RegistryEntry | undefined = registry.find(regEntry => regEntry.name === regEntryName);
+        const config: RegistryEntry | undefined = registry.find(regEntry => regEntry.name === regEntryName)
 
         if (!config) {
             console.error(`FATAL: could not find config for source ${regEntryName}. Aborted.`)
-            process.exit(1);
+            process.exit(1)
         }
 
+        await clone(config.entry, async outputLocation => {
+            const outputDir = (() => {
+                // copyTo is undefined or a directory
+                if (!config.copyTo?.match(/.+\.\w+$/)) {
+                    return config.copyTo ?? lamsalCnConfig.srcDirectory
+                }
+
+                const pathParts = config.copyTo!.split("/")
+                return pathParts.slice(0, pathParts.length - 1).join("/")
+            })()
+
+            if (!existsSync(outputDir)) {
+                mkdirSync(outputDir, { recursive: true })
+            }
+
+            const outputPath = cwdPath(outputDir, config.copyTo ?? config.entry.replace("/registry/", "")) // will be a directory
+            console.log(`Copying ${regEntryName} to ${outputPath}.`)
+
+            // files can be copied as-is to their output directory
+            if (statSync(outputLocation).isFile()) {
+                execSync(
+                    `mv ${outputLocation} ${cwdPath(outputDir, config.copyTo ?? config.entry.replace("/registry/", ""))}`,
+                )
+                return
+            }
+
+            const copySource = // if output location is already an existing directory, copy only the contents
+                existsSync(outputPath) && statSync(outputLocation).isDirectory()
+                    ? join(outputLocation, "*")
+                    : outputLocation
+
+            execSync(`mv ${copySource} ${outputPath}`)
+            return
+        }).then(() => {
+            console.log("Done.")
+        })
+
+        // CHECK FROM HERE AGAIN
         const conflictingDeps = getConflictingDeps(config, packageJson, "dependencies") ?? []
         const conflictingDevDeps = getConflictingDeps(config, packageJson, "devDependencies") ?? []
 
         if (conflictingDeps.length || conflictingDevDeps.length) {
-            const conflictsString = [...(conflictingDeps), ...conflictingDevDeps]
-                .map(({ name, current, next }) => `${name}: ${current} -> ${next}`).join("\n")
-            const confirmed = await confirm({ message: `Overwrite the following dependencies?\n${conflictsString}` })
-                .catch(() => process.exit(1))
+            const conflictsString = [...conflictingDeps, ...conflictingDevDeps]
+                .map(({ name, current, next }) => `${name}: ${current} -> ${next}`)
+                .join("\n")
+            const confirmed = await confirm({
+                message: `Overwrite the following dependencies?\n${conflictsString}`,
+            }).catch(() => process.exit(1))
 
             if (!confirmed) {
                 console.log("Not confirmed. Skipping.")
@@ -99,64 +166,23 @@ async function copySources(added: string[]) {
 
         overallDeps.push(...(config?.dependencies ?? []))
         overallDevDeps.push(...(config?.devDependencies ?? []))
-
-        const outputDir = (() => {
-            // copyTo is undefined or a directory
-            if (!config.copyTo?.match(/.+\.\w+$/)) {
-                return config.copyTo ?? lamsalCnConfig.srcDirectory
-            }
-
-            const pathParts = config.copyTo!.split("/")
-            return pathParts.slice(0, pathParts.length - 1).join("/")
-        })()
-
-        if (!existsSync(outputDir)) {
-            mkdirSync(outputDir, { recursive: true })
-        }
-
-        const relativeOutputPath = config.entry.replace(/^\/registry\//, `/`);
-
-        const outputPath = join(outputDir, relativeOutputPath);
-
-
-        console.log(`Copying ${regEntryName} to ${outputPath}.`)
-
-        const emitter = degit(repository + config.entry, {
-            cache: false,
-            force,
-            verbose,
-        })
-
-        const id = randomUUID()
-        const degitOutput = join(os.tmpdir(), `lamsalcn-${id}`)
-
-        try {
-            await emitter.clone(degitOutput)
-
-            const degitOutputIsFile = outputPath.match(/.+\.\w+$/)
-            // output is directory but does not yet exist
-            if (!degitOutputIsFile && !existsSync(outputPath)) {
-                mkdirSync(outputPath)
-            }
-
-            execSync(`mv ${degitOutput}${!degitOutputIsFile ? "/*" : ""} ${cwdPath(outputPath)}`, { stdio: "inherit" })
-        } finally {
-            rmSync(degitOutput, { recursive: true, force: true })
-        }
-
-        console.log("Done.")
     }
 
     const packageManager = lamsalCnConfig.packageManager
 
     if (overallDeps.length) {
+        console.log("Adding deps:", overallDeps.join(","))
         execSync(`${packageManager} add ${overallDeps.join(" ")}`, { cwd: process.cwd(), stdio: "inherit" })
     }
     if (overallDevDeps.length) {
-        execSync(`${packageManager} add ${(packageManager === "npm" ? "--save-dev" : "-D")} ${overallDevDeps.join(" ")}`, {cwd: process.cwd(), stdio: "inherit" })
+        console.log("Adding devDeps:", overallDeps.join(","))
+        execSync(
+            `${packageManager} add ${packageManager === "npm" ? "--save-dev" : "-D"} ${overallDevDeps.join(" ")}`,
+            { cwd: process.cwd(), stdio: "inherit" },
+        )
     }
 }
 
-await copySources(added);
+await copySources(added)
 
-process.exit(0);
+process.exit(0)
--- cli/get-conflicting-deps.ts
+++ cli/get-conflicting-deps.ts
@@ -1,34 +1,81 @@
-import type {RegistryEntry} from "../registry";
+import type { RegistryEntry } from "../registry"
 
-export function getConflictingDeps(config: RegistryEntry, packageJson: {
-    dependencies?: Record<string, string>;
-    devDependencies?: Record<string, string>
-}, key: keyof typeof packageJson) {
-    return config[key]?.reduce((acc, curr) => {
-        const [name, version = "latest"] = curr.split("@") as [string, string]
+export function compareDeps(
+    { dependencies = [], devDependencies = [] }: RegistryEntry,
+    {
+        dependencies: currentDependencies = {},
+        devDependencies: currentDevDependencies = {},
+    }: {
+        dependencies?: Record<string, string>
+        devDependencies?: Record<string, string>
+    },
+): { name: string; nextVersion: string; conflict: boolean; dev: boolean }[] {
+    const parseVersion = (version: string) => {
+        const [modifier, major = "0", minor = "0", patch = "0"] =
+            version.match(/(\D*)(\d)+\.(\d)+\.(\d)+.*/)?.slice(1) ?? []
 
-        const currentVersion = {
-            ...packageJson.dependencies,
-            ...packageJson.devDependencies
-        }[name]
+        if (!modifier) {
+            return
+        }
 
-        if (!currentVersion) {
+        const precision =
+            {
+                "~": "patch",
+                "^": "minor",
+            }[modifier] ?? "major"
+
+        return {
+            precision,
+            major,
+            minor,
+            patch,
+        }
+    }
+
+    return [...dependencies, ...devDependencies].map((dep, index) => {
+        const [name, nextVersion = "latest"] = dep.split("@") as [string, string | undefined]
+        const stats = { name, nextVersion, conflict: false, dev: index >= dependencies.length }
+
+        return stats
+    })
+}
+
+export function getConflictingDeps(
+    config: RegistryEntry,
+    packageJson: {
+        dependencies?: Record<string, string>
+        devDependencies?: Record<string, string>
+    },
+    key: keyof typeof packageJson,
+) {
+    return config[key]?.reduce(
+        (acc, curr) => {
+            const [name, version = "latest"] = curr.split("@") as [string, string]
+
+            const currentVersion = {
+                ...packageJson.dependencies,
+                ...packageJson.devDependencies,
+            }[name]?.replace(/^\D*/g, "")
+
+            if (!currentVersion) {
+                return acc
+            }
+
+            if (version === "latest") {
+                acc.push({ name, current: currentVersion, next: version })
+                return acc
+            }
+
+            // parse semantic versioning
+            const currentMajor = currentVersion.split(".")[0]!
+            const nextMajor = version.split(".")[0]
+
+            if (currentMajor !== nextMajor) {
+                acc.push({ name, current: currentVersion, next: version })
+            }
+
             return acc
-        }
-
-        if (version === "latest") {
-            acc.push({name, current: currentVersion, next: version})
-            return acc
-        }
-
-        // parse semantic versioning
-        const currentMajor = currentVersion.split(".")[0]!
-        const nextMajor = version.split(".")[0]
-
-        if (currentMajor !== nextMajor) {
-            acc.push({name, current: currentVersion, next: version})
-        }
-
-        return acc
-    }, [] as { name: string, current: string, next: string }[]);
+        },
+        [] as { name: string; current: string; next: string }[],
+    )
 }
